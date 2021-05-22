6fa5d3882c6d54d0515eca32fc6fd84c7be1acfc
John Fretz Magabilin <magabilinfrets@gmail.com>
2021-05-22 19:45:40 +0800

    Add project files.

---
+++ Tic Tac Toe Game/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void A1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void A2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void B3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void B1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
