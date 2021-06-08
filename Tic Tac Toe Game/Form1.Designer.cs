1492706e7585d80721435cf4a946d20bd5196c85
John Fretz Magabilin <magabilinfrets@gmail.com>
2021-06-08 21:58:23 +0800

    Enhanced Tic Tac Toe Game using methods

--- Tic Tac Toe Game/Form1.Designer.cs
+++ Tic Tac Toe Game/Form1.Designer.cs
@@ -158,7 +158,7 @@ namespace Tic_Tac_Toe_Game
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.ClientSize = new System.Drawing.Size(323, 386);
+            this.ClientSize = new System.Drawing.Size(323, 387);
             this.Controls.Add(this.btnNewGame);
             this.Controls.Add(this.C3);
             this.Controls.Add(this.B3);
